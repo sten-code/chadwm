2265e11abf60c49563902b87ab4ec9418715892d
sten <sten@gerben.home>
2023-12-30 15:07:23 +0100

    Fixed typo

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,7 +2,7 @@
 
 xrdb merge ~/.config/chadwm/.Xresources 
 xbacklight -set 10 &
-feh --bg-fill ~/.wallpaper/wallpaper.png &
+feh --bg-fill ~/.wallpapers/wallpaper.png &
 xset r rate 200 50 &
 picom &
 
